ca53fd64c23edee56886cc5c06f2c13b528891ed
tonberisha1 <berishaelton03@gmail.com>
2026-05-24 22:51:57 +0100

    Create script.js
    Add calculator functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+function add() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let sum = num1 + num2;
+
+    document.getElementById("result").innerText = "Result: " + sum;
+}
